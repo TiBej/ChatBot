717761287c0b8a05eb4b2eded1fdcb3376968c71
Tim Elsener <TiBej@github.com>
2017-02-11 21:30:02 +0100

    Structure Update

---
+++ ChatBot/DiscoBot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Discord;
+using Discord.Commands;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot
+{
+    class DiscoBot
+    {
+        DiscordClient _DiscordClient;
+
+        public DiscoBot()
+        {
+            _DiscordClient = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            _DiscordClient.UsingCommands(x =>
+            {
+                x.PrefixChar = '+';
+                x.AllowMentionPrefix = true;
+            });
+
+            var commands = _DiscordClient.GetService<CommandService>();
+
+            commands.CreateCommand("+")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("Hi");
+                });
+
+            _DiscordClient.ExecuteAndWait(async () =>
+            {
+                await _DiscordClient.Connect("", TokenType.Bot);
+            });
+        }
+
+        private void Log(Object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
--- README.md
+++
@@ -1,5 +0,0 @@
-# DiscordEssentials
-
-A simple Discord Bot which adds random features to Discord :P 
-
-Enjoy!
