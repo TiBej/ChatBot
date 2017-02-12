063e90c5297d00f0feb2a743bba2202bcdc8c67d
Tim Elsener <TiBej@github.com>
2017-02-12 12:10:49 +0100

    First working Version

--- ChatBot/DiscoBot.cs
+++ ChatBot/DiscoBot.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AIMLbot;
 
 namespace ChatBot
 {
@@ -14,10 +15,22 @@ namespace ChatBot
 
         public DiscoBot()
         {
+            // Setup AIML
+            Bot AI = new Bot();
+            AI.loadSettings();
+            AI.loadAIMLFromFiles();
+            AI.isAcceptingUserInput = false;
+
+            // Setup User
+            AIMLbot.User myUser = new AIMLbot.User("Username", AI);
+
+            AI.isAcceptingUserInput = true;
+
+            // Discord Settings
             _DiscordClient = new DiscordClient(x =>
             {
                 x.LogLevel = LogSeverity.Info;
-                x.LogHandler = Log;
+                x.LogHandler = log;
             });
 
             _DiscordClient.UsingCommands(x =>
@@ -28,19 +41,46 @@ namespace ChatBot
 
             var commands = _DiscordClient.GetService<CommandService>();
 
-            commands.CreateCommand("+")
+            commands.CreateCommand("help")
                 .Do(async (e) =>
                 {
-                    await e.Channel.SendMessage("Hi");
+                    await e.Channel.SendMessage("Get help with +help <text>");
+                    await e.Channel.SendMessage("Send a message with ++ <text>");
+                    await e.Channel.SendMessage("Check Version with +version <text>");
                 });
+
+            commands.CreateCommand("+")
+                .Parameter("words", ParameterType.Unparsed)
+                .Do(async (e) =>
+                {
+                    // Get text
+                    string s = e.GetArg("words");
+
+                    // Send request
+                    Request r = new Request(s, myUser, AI);
+
+                    // Save answer
+                    Result res = AI.Chat(r);
+
+                    // Output answer
+                    await e.Channel.SendMessage(res.Output);
+                });
+
+            commands.CreateCommand("version")
+             .Do(async (e) =>
+             {
+                    // Send Version
+                    await e.Channel.SendMessage("Chatter v1.0");
+            });
 
             _DiscordClient.ExecuteAndWait(async () =>
             {
-                await _DiscordClient.Connect("", TokenType.Bot);
+                // Change Token
+                await _DiscordClient.Connect("MjgwMTA0NjMzNzIzMjU2ODM0.C4Ej7w.mZIhU7SKJqrWgfOo-quqdKIRj5c", TokenType.Bot);
             });
         }
 
-        private void Log(Object sender, LogMessageEventArgs e)
+        private void log(Object sender, LogMessageEventArgs e)
         {
             Console.WriteLine(e.Message);
         }
--- ChatBot/Program.cs
+++ ChatBot/Program.cs
@@ -13,25 +13,6 @@ namespace ChatBot
         {
             // Create Bot
             DiscoBot TestBot = new DiscoBot();
-
-            // Setup AIML
-            Bot AI = new Bot();
-            AI.loadSettings();
-            AI.loadAIMLFromFiles();
-            AI.isAcceptingUserInput = false;
-
-            // Setup User
-            User myUser = new User("Username", AI);
-
-            AI.isAcceptingUserInput = true;
-
-            // Send and receive message
-            while (true)
-            {
-                Request r = new Request(Console.ReadLine(), myUser, AI);
-                Result res = AI.Chat(r);
-                Console.WriteLine("Robot: " + res.Output);
-            }
         }
     }
 }
